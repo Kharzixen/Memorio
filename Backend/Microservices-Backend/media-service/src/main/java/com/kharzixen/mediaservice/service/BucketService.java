fbf25934e29df597a3f94ef9a07e9220808d4a22
Kharzixen <mellau.mark@gmail.com>
2024-06-10 17:37:05 +0300

    media service startup hotfix

--- Backend/Microservices-Backend/media-service/src/main/java/com/kharzixen/mediaservice/service/BucketService.java
+++ Backend/Microservices-Backend/media-service/src/main/java/com/kharzixen/mediaservice/service/BucketService.java
@@ -1,14 +1,15 @@
 package com.kharzixen.mediaservice.service;
 
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
+import io.minio.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
@@ -31,9 +32,37 @@ public class BucketService {
                     minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
             if (!found) {
                 minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+                if(Objects.equals(bucketName, "public-album-bucket") || Objects.equals(bucketName, "private-album-bucket")){
+                    InputStream image1Stream;
+                    image1Stream = getClass().getResourceAsStream("/default_album_image.jpg");
+                    assert image1Stream != null;
+                    PutObjectArgs args = PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object("default.album_image.jpg")
+                            .stream(image1Stream, image1Stream.available(), -1)
+                            .contentType("image/jpeg")
+                            .build();
+                    ObjectWriteResponse resp = minioClient.putObject(args);;
+                    log.info("Image {} uploaded to {}", resp.object(), bucketName);
+                }
+
+                if(Objects.equals(bucketName, "profile-bucket")){
+                    InputStream image1Stream;
+                    image1Stream = getClass().getResourceAsStream("/default_pfp_id.jpg");
+                    assert image1Stream != null;
+                    PutObjectArgs args = PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object("default_pfp_id.jpg")
+                            .stream(image1Stream, image1Stream.available(), -1)
+                            .contentType("image/jpeg")
+                            .build();
+                    ObjectWriteResponse resp = minioClient.putObject(args);;
+                    log.info("Image {} uploaded to {}", resp.object(), bucketName);
+                }
             } else {
                 log.info("Bucket {} already exists.", bucketName);
             }
+
         } catch (Exception e) {
             log.error("Error while creating bucket '{}': {}", bucketName, e.getMessage(), e);
         }
